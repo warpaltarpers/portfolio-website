1504847cc0e0b7e21a7c5d5c81c364237c34aef2
Max Walters <max.walters10@gmail.com>
2022-03-12 17:18:44 -0500

    Configuring Tailwind

---
+++ src/Portfolio.js
@@ -0,0 +1,15 @@
+import './Portfolio.css';
+
+function Portfolio() {
+  return (
+    <div className='Portfolio'>
+      <section id='landing'></section>
+      <section id='about'></section>
+      <section id='exp'></section>
+      <section id='work'></section>
+      <section id='contact'></section>
+    </div>
+  );
+}
+
+export default Portfolio;
