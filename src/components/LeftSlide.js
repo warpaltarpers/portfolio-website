193bf48bf083fe790911a91389a8098c30b00a87
Max Walters <max.walters10@gmail.com>
2022-03-13 14:11:42 -0400

    Adjust padding at large breakpoint

--- src/components/LeftSlide.js
+++ src/components/LeftSlide.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LeftSlide = props => {
   return (
-    <div id='landing' className='snap-start h-screen w-screen bg-heavy-metal flex flex-col items-start text-left justify-center pl-6 lg:pl-12 ease-in duration-200'>
+    <div id='landing' className='snap-start h-screen w-screen bg-heavy-metal flex flex-col items-start text-left justify-center pl-6 lg:pl-16 ease-in duration-200'>
       {props.content}
     </div>
   )
--- src/components/RightSlide.js
+++ src/components/RightSlide.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const RightSlide = (props) => {
   return (
-    <div id='landing' className='snap-start h-screen w-screen bg-heavy-metal flex flex-col items-end text-right justify-center pr-6 lg:pr-12 ease-in duration-200'>
+    <div id='landing' className='snap-start h-screen w-screen bg-heavy-metal flex flex-col items-end text-right justify-center pr-6 lg:pr-16 ease-in duration-200'>
       {props.content}
     </div>
   )
